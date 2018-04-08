beddbd0e8cae590fdd15f684907651cfd108ce2b
njang <neo.w.jang@gmail.com>
2018-04-08 08:59:13 -0500

    Refactor App.js file

--- src/App.js
+++ src/App.js
@@ -5,28 +5,29 @@ import Instruction from './components/Modals/Instruction'
 import WinMessage from './components/Modals/WinMessage'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Word Twist</h1>
-    <form autocomplete="off">
-      <input id="guess" type="text" placeholder="Enter text here" autofocus required></input>
-      <div id="buttonRow">
-        <button type="submit" class="btn btn-primary" id="submit">Submit</button>
-        <button type="button" class="btn btn-success" id="twist">Twist</button>
-        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalReset">Reset</button>
-        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modalInfo">How to Play</button>  
-      </div>      
-    </form>
-    <div id="twisted"></div>
-    <div id="words" class="text-justify d-inline"></div>
-    <Reset />
-    <Instruction />
-    <WinMessage />
+    render() {
+        return (
+            <div className="App">
+                <h1>Word Twist</h1>
+                <form autoComplete="off">
+                    <input id="guess" type="text" placeholder="Enter text here" autoFocus required></input>
+                    <div id="buttonRow">
+                        <button type="submit" className="btn btn-primary" id="submit">Submit</button>
+                        <button type="button" className="btn btn-success" id="twist">Twist</button>
+                        <button type="button" className="btn btn-danger" data-toggle="modal" data-target="#modalReset">Reset</button>
+                        <button type="button" className="btn btn-info" data-toggle="modal" data-target="#modalInfo">How to Play</button>  
+                    </div>      
+                </form>
+                <div id="twisted"></div>
+                <div id="words" className="text-justify d-inline"></div>
 
-      </div>
-    );
-  }
+                <Reset />
+                <Instruction />
+                <WinMessage />
+
+            </div>
+        );
+    }
 }
 
 export default App;
