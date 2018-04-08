5d3947ae8f515860829ee0455afc95d7e36831bf
njang <neo.w.jang@gmail.com>
2018-04-08 08:58:56 -0500

    Address React warning messages

--- src/components/Modals/Instruction.js
+++ src/components/Modals/Instruction.js
@@ -3,22 +3,22 @@ import React, { Component } from 'react';
 class Instruction extends Component {
 	render() {
 		return (
-			<div id="modalInfo" class="modal fade" role="dialog">
-				<div class="modal-dialog modal-md">
+			<div id="modalInfo" className="modal fade" role="dialog">
+				<div className="modal-dialog modal-md">
 					{/* Modal content*/}
-					<div class="modal-content">
-						<div class="modal-header float-center">
+					<div className="modal-content">
+						<div className="modal-header float-center">
 							<h2>How to Play</h2>
 						</div>
-						<div class="modal-body text-left">
+						<div className="modal-body text-left">
 							<ul>
 								<li>Using keyboard, enter your guess and press enter.</li>
 								<li>Press <i>Twist</i> button to scramble the order of letters.</li>
 								<li>Press <i>Reset</i> button to start another game.</li>
 							</ul>
 						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-default" data-dismiss="modal" default>Close</button>
+						<div className="modal-footer">
+							<button type="button" className="btn btn-default" data-dismiss="modal" default>Close</button>
 						</div>
 					</div>
 				</div>
--- src/components/Modals/WinMessage.js
+++ src/components/Modals/WinMessage.js
@@ -3,23 +3,23 @@ import React, { Component } from 'react';
 class winMessage extends Component {
 	render() {
 		return (
-			<div id="modalWin" class="modal fade" role="dialog">
-				<div class="modal-dialog modal-md">
+			<div id="modalWin" className="modal fade" role="dialog">
+				<div className="modal-dialog modal-md">
 					{/* Modal content*/}
-					<div class="modal-content">
-						<div class="modal-header float-center">
+					<div className="modal-content">
+						<div className="modal-header float-center">
 							<h2>You Win!</h2>
 						</div>
-						<div id="winMessage" class="modal-body text-left">
+						<div id="winMessage" className="modal-body text-left">
 							{/* <ul>
 							<li>Using keyboard, enter your guess and press enter.</li>
 							<li>Press <i>Twist</i> button to scramble the order of letters.</li>
 							<li>Press <i>Reset</i> button to start another game.</li>
 							</ul> */}
 						</div>
-						<div class="modal-footer">
-							<button type="button" class="reset btn btn-danger" data-dismiss="modal">Play another</button>
-							<button type="button" class="btn btn-default" data-dismiss="modal" default>Close</button>
+						<div className="modal-footer">
+							<button type="button" className="reset btn btn-danger" data-dismiss="modal">Play another</button>
+							<button type="button" className="btn btn-default" data-dismiss="modal" default>Close</button>
 						</div>
 					</div>
 				</div>
